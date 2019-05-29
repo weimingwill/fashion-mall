00d12944cd36cef46e9127c8abdc3ba286dab521
Weiming <wingalong@gmail.com>
2019-05-30 00:31:09 +0800

    Implement home page

--- src/api/api.js
+++ src/api/api.js
@@ -9,7 +9,16 @@ const wxJsCode2Session = (params) => wxRequest(params, apiMall + "/api/wechat/js
 //用户是否绑定手机号
 const getUserInfo = (params) => wxRequest(params, apiMall + '/api/userCenter/getUserInfo');
 
+
+//商品接口
+//首页发现商品接口
+const getHomeDisvocerList = (params) => wxRequest(params, apiMall + '/api/mall/discoverList');
+//查询广告列表
+const getAdList = (params) => wxRequest(params, apiMall + '/api/adverts/list');
+
 export default {
   wxJsCode2Session,
-  getUserInfo
+  getUserInfo,
+  getHomeDisvocerList,
+  getAdList,
 }
